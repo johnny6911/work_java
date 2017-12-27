355c158bddca1bb54528860aa3f019355ab0d733
KOITT <KOITT@KOITT-PC18>
2017-12-27 15:33:18 +0900

    for 반복문 예제소스

---
+++ ch04/src/control/loop/For01.java
@@ -0,0 +1,18 @@
+package control.loop;
+
+public class For01 {
+
+		public static void main(String[] args) {
+			//마지막 i값은 10이 된다
+			for (int i = 0; i < 10; i++) {
+		
+			}
+			//i는 for문과 그 바디 안에서만 쓸수있다
+			//System.out.println(i);
+			
+			for (int i = 0; i < 10; i++) {
+				
+			}
+		}
+	
+}
