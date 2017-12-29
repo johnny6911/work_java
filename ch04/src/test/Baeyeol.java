5efe5a9b2ab8ee27681e1991485c7e9f7742b862
KOITT <KOITT@KOITT-PC18>
2017-12-29 15:19:34 +0900

    연습문제 4장

--- ch04/src/test/Baeyeol.java
+++ ch04/src/test/Baeyeol.java
@@ -7,15 +7,15 @@ public class Baeyeol {
 		Scanner input = new Scanner(System.in);
 		System.out.println("숫자 입력 >> ");
 		int a = input.nextInt();
-		
+
 		// 행을 만들어주는 과정	.char b배열의 대한 a의 값은 length가 된다
 		char b[][] = new char[a][];
-		
+
 		// 1. 열을 만들어주는 과정
 		for (int i=0; i<b.length; i++) {
-			 b[i] = new char[i + 1];
+			b[i] = new char[i + 1];
 		}
-		
+
 		// 2. 열안에 *을 대입해주는 과정
 		for (int i=0; i<b.length; i++) {
 			for (int j=0; j < b[i].length; j++) {
@@ -24,15 +24,15 @@ public class Baeyeol {
 			}
 			//System.out.println();
 		}
-//		3. *을 열에 출력해주는 과정
+		//		3. *을 열에 출력해주는 과정
 		for (int i=0; i<b.length; i++) {
 			for (int j=0; j <= i; j++) {
 				System.out.print(b[i][j]);
 			}
 			System.out.println();
 		}
-		
-		
+
+
 		input.close();
 	}
 }
--- ch04/src/test/Testopposite.java
+++ ch04/src/test/Testopposite.java
@@ -5,6 +5,7 @@ public class Testopposite {
 		
 		int i, j;
 		
+		// 곱하는 수를 고정 i와 j 앞에 int타입을 선언하여도 된다
 		for ( i= 1; i <= 9; i++) {
 			for (j = 2; j <= 9; j++) {
 				System.out.printf("%d * %d =%02d "+"  ", j, i, i * j);
