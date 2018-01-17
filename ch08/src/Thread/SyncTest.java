bd7d96e691d8371dadf889587f5da884944652e9
KOITT <KOITT@KOITT-PC18>
2018-01-17 16:21:38 +0900

    8장 333쪽까지 Sync 동기화후

--- ch08/src/Thread/SyncTest.java
+++ ch08/src/Thread/SyncTest.java
@@ -6,7 +6,7 @@ class BankAccount{
 	int balance = 0; // 은행잔고
 	
 	// 인출
-	public synchronized void withdraw(int money) {
+	public synchronized void withdraw(int money) { // withdraw 메소드 전체를 임계영역으로 잡음
 		// 현재 출금하려는 금액보다 잔고가 많은지 검사
 		if (money >= 0 && balance >= money) {
 			try {
@@ -25,13 +25,13 @@ class BankAccount{
 	
 	// 입금
 	public void deposit(int money) {
-		synchronized (this) {
+		synchronized (this) { // 특정 영역을 임계영역으로 잡음(중괄호 시작부터 중괄호 끝나는 부분까지 임계영역으로 설정)
 			if(money > 0) {
 				balance += money;
 				System.out.printf("%s %d 입금하여 현재잔고 %d입니다 %n", 
 					Thread.currentThread().getName(), money, balance);
 			}
-		}
+		} // 여기까지가 임계영역(중괄호)의 끝나는 부분
 	}
 }
 
@@ -39,10 +39,10 @@ public class SyncTest implements Runnable{ // 러너블 상속받고있음
 	BankAccount act = new BankAccount();
 	
 	public void run() { //런이라는 메소드 구현후 스레드가 해야할일을 만들어줌
-		while (true) { //와일문 안에있는 영역을 Critical Section(임계 영역)
+		for (int i = 0; i < 3; i++) {
 			int amount = new Random().nextInt(10000); // 0부터 10000까지 랜덤한 숫자를 10으로 나눈 나머지에 1000으로 곱해서 나옴
 			amount = amount%10 * 1000; //10으로 나눈 나머지이기 때문에 1의 자리값에 1000을 곱한 값
-			System.out.printf("[%s] 금액 = %d %n", Thread.currentThread().getName(), amount);
+			System.out.printf("[%s] 금액 = %d %n", Thread.currentThread().getName(), amount); // 가진 돈
 			act.deposit(amount); 	//입금
 			act.withdraw(amount*2); //출금 (2배로 한 이유는 잔고를 바닥나게 하기 위해서)
 			if(act.balance < 0) {
@@ -58,8 +58,8 @@ public class SyncTest implements Runnable{ // 러너블 상속받고있음
 		Thread th1 = new Thread(r);
 		Thread th2 = new Thread(r);
 		
-		th1.setName("철수");
-		th2.setName("영희");
+		th1.setName("철수가");
+		th2.setName("영희가");
 		
 		th1.start();
 		th2.start();
